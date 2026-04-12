1389849ad1f76d20d7e70d5356dea83b23909ef1
Mahmoud Ehab <mahmoud.ehab.moheb.dev@gmail.com>
2026-04-13 00:36:59 +0200

    Changed the project structure to make it work with the GUI

---
+++ OSproject/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module mahmoudehabmoheb.osprojectnew {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens mahmoudehabmoheb.osprojectnew to javafx.fxml;
+    exports mahmoudehabmoheb.osprojectnew;
+}
