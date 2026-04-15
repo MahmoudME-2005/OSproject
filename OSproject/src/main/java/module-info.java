d9671f9bdd1b449255a8ddf8d088de6e5347acfe
salsabil <salsabilalimahmoud@gmail.com>
2026-04-15 15:33:16 +0200

    Al7a2ona3

--- OSproject/src/main/java/module-info.java
+++ OSproject/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module mahmoudehabmoheb.osprojectnew {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
     opens mahmoudehabmoheb.osprojectnew to javafx.fxml;
     exports mahmoudehabmoheb.osprojectnew;
