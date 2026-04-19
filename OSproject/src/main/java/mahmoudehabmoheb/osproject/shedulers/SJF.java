cef6811617e8f386e1625d09fd56833cdbb923d3
Mahmoud Ehab <mahmoud.ehab.moheb.dev@gmail.com>
2026-04-19 14:16:49 +0200

    Final matching.

--- OSproject/src/main/java/mahmoudehabmoheb/osproject/shedulers/SJF.java
+++ OSproject/src/main/java/mahmoudehabmoheb/osproject/shedulers/SJF.java
@@ -5,7 +5,6 @@ import mahmoudehabmoheb.osproject.Process;
 import java.util.Comparator;
 import java.util.List;
 import java.util.PriorityQueue;
-import java.util.concurrent.CountDownLatch;
 import javafx.application.Platform;
 
 /**
